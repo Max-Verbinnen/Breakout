0c29f7c60e919d0c8409fdbeb3bcdbb039e474d4
Sander Verwimp <70103188+sander-verwimp@users.noreply.github.com>
2022-02-23 22:05:32 +0100

    Begin implementation BreakoutState

--- Breakout - opgave 1/src/breakout/BreakoutState.java
+++ Breakout - opgave 1/src/breakout/BreakoutState.java
@@ -2,28 +2,54 @@ package breakout;
 
 // TODO: implement, document
 public class BreakoutState {
-
+	
+	private BallState[] balls;
+	private BlockState[] blocks;
+	private Point bottomRight;
+	private PaddleState paddle;
+ 
 	
 	public BreakoutState(BallState[] balls, BlockState[] blocks, Point bottomRight, PaddleState paddle) {
+		if (balls == null || blocks == null || bottomRight == null || paddle == null) {
+			throw new IllegalArgumentException("Not a valid argument.");
+		}
+		this.balls = balls;
+		this.blocks = blocks;
+		this.bottomRight = bottomRight;
+		this.paddle = paddle;
 	}
 	
 	public BallState[] getBalls() {
-		return null;
+		return balls;
 	}
 
 	public BlockState[] getBlocks() {
-		return null;
+		return blocks;
 	}
 
 	public PaddleState getPaddle() {
-		return null;
+		return paddle;
 	}
 
 	public Point getBottomRight() {
-		return null;
+		return bottomRight;
 	}
 
 	public void tick(int paddleDir) {
+		
+		// Move all balls one step forward according to their current velocity.
+		for (int i = 0; i < balls.length; i++) {
+			BallState ball = balls[i];
+			balls[i] = new BallState(ball.getCenter().plus(ball.getVelocity()), ball.getDiameter(), ball.getVelocity());
+			
+			ball = balls[i];
+			if(ball.getCenter().minus(new Vector(ball.getDiameter() / 2, 0)).getX() <= 0) {
+				balls[i] = new BallState(ball.getCenter(), ball.getDiameter(), ball.getVelocity().mirrorOver(Vector.UP));
+			}
+		}
+		
+		// Check whether any balls hit the walls on the left, right and top side of the game area, in which case they must bounce back.
+
 	}
 
 	public void movePaddleRight() {
