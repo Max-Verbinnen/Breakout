6163f5f6ed9ab713c1b672b72527086268f6c2d3
Max <max.verbinnen@skynet.be>
2022-03-03 15:37:59 +0100

    Continue working on BreakoutState

--- Breakout - opgave 1/src/breakout/BreakoutState.java
+++ Breakout - opgave 1/src/breakout/BreakoutState.java
@@ -1,5 +1,7 @@
 package breakout;
 
+import java.util.Arrays;
+
 // TODO: implement, document
 public class BreakoutState {
 	
@@ -13,6 +15,7 @@ public class BreakoutState {
 		if (balls == null || blocks == null || bottomRight == null || paddle == null) {
 			throw new IllegalArgumentException("Not a valid argument.");
 		}
+
 		this.balls = balls.clone();
 		this.blocks = blocks.clone();
 		this.bottomRight = bottomRight;
@@ -20,11 +23,11 @@ public class BreakoutState {
 	}
 	
 	public BallState[] getBalls() {
-		return balls;
+		return balls.clone();
 	}
 
 	public BlockState[] getBlocks() {
-		return blocks;
+		return blocks.clone();
 	}
 
 	public PaddleState getPaddle() {
@@ -36,19 +39,19 @@ public class BreakoutState {
 	}
 
 	public void tick(int paddleDir) {
-		
+
 		// Move all balls one step forward according to their current velocity.
 		for (int i = 0; i < balls.length; i++) {
-			
+
 			BallState ball = balls[i];			
 			balls[i] = new BallState(ball.getCenter().plus(ball.getVelocity()), ball.getDiameter(), ball.getVelocity());
 		}
 		
 		// Check whether any balls hit the walls on the left, right and top side of the game area, in which case they must bounce back.
 		for (int i = 0; i < balls.length; i++) {
-			
+
 			BallState ball = balls[i];
-			int radius = ball.getDiameter()/2;
+			int radius = ball.getDiameter() / 2;
 			
 			int centerToLeftBound = ball.getCenter().getX();
 			int centerToTopBound = ball.getCenter().getY();
@@ -68,45 +71,31 @@ public class BreakoutState {
 			}
 		}
 		
-		//Check whether any balls hit the bottom of the field, in which case they must be removed from the game.
-		int deletedBalls = 0;
-		for (int i = 0; i < balls.length; i++) {
-			
-			BallState ball = balls[i];
-			int radius = ball.getDiameter()/2;
-			
-			int centerToBottomBound = bottomRight.getY() - ball.getCenter().getY();
-
-			if (centerToBottomBound <= radius) {
-				balls[i] = null;
-				deletedBalls++;
-			}
-		}
-		if(deletedBalls > 0) {
-			BallState[] newBalls = new BallState[balls.length - deletedBalls];
-			int j = 0;
-			for (int i = 0; i < balls.length; i++) {
-				if (balls[i] != null) {
-					newBalls[j] = balls[i];
-					j++;
-				}	
-			}
-			balls = newBalls;
-		}
+		// Check whether any balls hit the bottom of the field, in which case they must be removed from the game.
+		balls = Arrays.stream(balls)
+					  .filter(ball -> bottomRight.getY() - ball.getCenter().getY() > ball.getDiameter() / 2)
+					  .toArray(BallState[]::new);
+		
+		// TODO: Check whether any ball hit any block, in which case the block must be removed from the game and the ball must bounce back.
+		
+		// TODO: Check whether any ball hit the paddle, in which case it must bounce back.
+		// Additionally, the ball must speed up by one fifth of the current velocity of the paddle.
 		
 	}
 
 	public void movePaddleRight() {
+		paddle = new PaddleState(paddle.getCenter().plus(new Vector(10, 0)));
 	}
 
 	public void movePaddleLeft() {
+		paddle = new PaddleState(paddle.getCenter().minus(new Vector(10, 0)));
 	}
 	
 	public boolean isWon() {
-		return false;
+		return balls.length >= 1 && blocks.length == 0;
 	}
 
 	public boolean isDead() {
-		return false;
+		return balls.length == 0;
 	}
 }
