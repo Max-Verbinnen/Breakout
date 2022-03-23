75a3480a0bb15646d94085314c968cdd201b41a7
Sander Verwimp <70103188+sander-verwimp@users.noreply.github.com>
2022-03-23 14:26:23 +0100

    Full coverage for Rectangle

--- Breakout - opgave 1/src/breakout/RectangleTest.java
+++ Breakout - opgave 1/src/breakout/RectangleTest.java
@@ -1,6 +1,9 @@
 package breakout;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,11 +11,22 @@ import org.junit.jupiter.api.Test;
 class RectangleTest {
 	Rectangle r1;
 	Rectangle r2;
+	BallState[] balls1 = new BallState[5];
+	BallState[] balls2 = new BallState[2];
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		r1 = new Rectangle(new Point(10, 11), new Point(30, 31), false);
 		r2 = new Rectangle(new Point(1, 7), new Point(100, 200), true);
+		
+		balls1[0] = new BallState(new Point(15, 10), 4, new Vector(2, 7));
+		balls1[1] = new BallState(new Point(15, 29), 4, new Vector(2, 7));
+		balls1[2] = new BallState(new Point(15, 1), 4, new Vector(2, 7));
+		balls1[3] = new BallState(new Point(9, 20), 4, new Vector(2, 7));
+		balls1[4] = new BallState(new Point(15, 32), 4, new Vector(2, -7));
+		
+		balls2[0] = new BallState(new Point(99, 150), 4, new Vector(2, 7));
+		balls2[1] = new BallState(new Point(101, 150), 4, new Vector(-2, 7));
 	}
 	
 	@Test
@@ -31,6 +45,21 @@ class RectangleTest {
 		assertEquals(Vector.UP, r2.getTopBoundVector());
 		assertEquals(Vector.RIGHT, r2.getRightBoundVector());
 		assertEquals(Vector.DOWN, r2.getBottomBoundVector());
+	}
+	
+	@Test
+	void testCollide() {
+		assertArrayEquals(r1.collide(balls1, false), new int[] {0, 3, 4});
+		assertArrayEquals(r2.collide(balls2, false), new int[] {0});
+	}
+	
+	@Test
+	void testCollideOnce() {
+		assertArrayEquals(r1.collide(balls1, true), new int[] {0});
+		assertArrayEquals(r1.collide(balls1, true), new int[] {3});
+		assertArrayEquals(r1.collide(balls1, true), new int[] {4});
+
+		assertArrayEquals(r2.collide(balls2, true), new int[] {0});
 	}
 
 }
